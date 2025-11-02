8d589d57b0fea64fcf8c48a3d9b0db86538166b6
Sumiti1308 <sumiti25csu318@ncuindia.edu>
2025-11-02 23:11:03 +0530

    Create Q14.c

---
+++ Q14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+        if (isPrime(arr[i])) count++;
+
+    printf("Number of prime numbers: %d\n", count);
+    return 0;
+}
